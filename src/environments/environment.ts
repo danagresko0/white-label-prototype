24f67d6a90a7737481a47616aa23aa035ae3294a
danagresko0 <dana.a.gresko@ey.com>
2026-04-16 19:02:46 -0400

    Remove

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,18 +6,18 @@ import { Environment } from './environment.model';
 export const environment: Environment = {
   production: false,
   okta: {
-    orgUrl: 'https://trial-2653197.okta.com',
-    issuer: 'https://trial-2653197.okta.com/oauth2/default',
-    clientId: '0oa11ty2gqgZjMqYK698',
-    webClientId: '0oa11v7t2inOYySA7698',
-    nativeClientId: '0oa11ty2gqgZjMqYK698',
-    webRedirectUri: 'http://localhost:4206/login/callback',
-    webPostLogoutRedirectUri: 'http://localhost:4206/home',
-    nativeRedirectUri: 'com.okta.trial-2653197:/callback',
-    nativePostLogoutRedirectUri: 'com.okta.trial-2653197:/',
-    scopes: ['openid', 'profile', 'email'],
+    orgUrl: '',
+    issuer: '',
+    clientId: '',
+    webClientId: '',
+    nativeClientId: '',
+    webRedirectUri: '',
+    webPostLogoutRedirectUri: '',
+    nativeRedirectUri: '',
+    nativePostLogoutRedirectUri: '',
+    scopes: [],
     pkce: true,
-    apiToken: '00PE04ki_8PZpSut2IBXSUTuHZ1zDT_kJNOT6f7m0i'
+    apiToken: ''
   }
 };
 
